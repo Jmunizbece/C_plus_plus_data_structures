f0e2ce562e47cebb30c30a473a46325d250f9521
Jmunizbece <jmunizbece@icloud.com>
2021-03-26 20:07:39 -0400

    Addition of Switch Cases
    These switch cases allow for full implementation of stack data structure and to be capable of responding to user input.

--- Stack.cpp
+++ Stack.cpp
@@ -92,7 +92,60 @@ int main()
 		std::cout << "6. count()" << std::endl;
 		std::cout << "7. change()" << std::endl;
 		std::cout << "8. display()" << std::endl;
-		std::cout << "9. Clear Screen" << std::endl; 
+		std::cout << "9. Clear Screen" << std::endl;
+
+		std::cin >> option;
+		switch(option)
+		{
+			case 1:
+				std::cout << "Enter an iem to push in the stack" << std::endl;
+				std::cin >> value;
+				s1.push(value);
+				break;
+			case 2:
+				std::cout << "Popped function called. Popped value is: " << s1.pop() << std::endl;
+				break;
+			case 3:
+				if (s1.isEmpty()) {
+					std::cout << "Stack is empty." << std::endl;
+				} else {
+					std::cout << "Stack is not empty." << std::endl;
+				} break; 
+			case 4:
+				if (s1.isFull()) {
+					std:: cout << "Stack is full." << std::endl;
+				} else {
+					std::cout << "Stack is not full." << std::endl;
+				} break; 
+			case 5:
+				std::cout << "Enter Position you wish to peek at: " << std::endl;
+				std::cin >> position;
+				std::cout << "The value at specified position is: " << s1.peek(position) << std::endl;
+				break;
+			case 6:
+				std::cout << "Count function called. The total number of items is: " << s1.count() << std::endl;
+
+			case 7:
+				std::cout << "Please enter the position you wish to change: " <<std::endl;
+				std::cin >> position;
+				
+				std::cout << "Please enter the value you wish to replace the old value with: " << std::endl;
+				std::cin >> value;
+
+				s1.change(position, value);
+
+				std::cout << "The position's value has been updated. The new value at position " << position << " has been updated to " << value << std::endl;
+				break;
+			case 8:
+				std::cout << "Display function called - " << std::endl;
+				s1.display();
+				break;
+			case 9:
+				system("cls");
+				break;
+			default:
+				std::cout << "Please enter the number of operation you wish to perform: " << std::endl;
+		}
 		
 	} while(option != 0) {
 	}
